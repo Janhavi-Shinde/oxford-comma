4f8278351ec30ed64aa49fd74475c9527a1bbfa0
janhavi <janashinde394@gmail.com>
2022-05-15 00:28:05 +1000

    all tests passing

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
+# fruits = ['strawbery', 'banana', 'dates', 'lucky', 'sourav', 'kher']
+# def oxford_comma(array)
+#     case array.size
+#     when 1
+#         puts array.join
+#     when 2 
+#         puts array.join(' and ')
+#     when array.size >= 3
+#         puts array.join(' , ')
+#     end         
+# end
 def oxford_comma(array)
-
+    if array.size == 1
+         array.join
+    elsif array.size == 2 
+         array.join(' and ')
+    elsif array.size >= 3
+        
+          array[0..-2].join(', ') << ', and ' << array[-1] 
+        
+    end         
 end
+
+#  puts fruits[-2] << ' and ' << fruits[-1] 
+
